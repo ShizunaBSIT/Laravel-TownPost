9fbd1763b862034016fabbe787da7a3e6fccbb6f
mknthnm <mmnmillano@tip.edu.ph>
2024-11-22 21:09:14 +0800

    Adding of Login functions

--- townPost_laravel/routes/web.php
+++ townPost_laravel/routes/web.php
@@ -2,10 +2,17 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Login routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
