45c50e9d6e3e4c6a75bc9506873e7143824e2d7b
itsmeEn <166961047+itsmeEn@users.noreply.github.com>
2024-11-24 13:40:16 +0800

    additional file landing.blade.php

--- townPost_laravel/resources/views/landing.blade.php
+++ townPost_laravel/resources/views/landing.blade.php
@@ -9,12 +9,8 @@
     <link href="{{ asset('/css/announcement.css') }}" rel="stylesheet">
 </head>
 <body>
-
-<!-- Navbar -->
-<!--Navbar-->
 <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
   <div class="container-fluid">
-       <img src = "/images/Logo.jpg" width ="30" height="30" alt = "Logo">
     <div class="collapse navbar-collapse" id="search">
         <form class="form-inline">
                 <input class="form-control" type="search" placeholder="Search Here" aria-label="Search">
@@ -22,20 +18,12 @@
                     <i class="bi bi-search"></i>
                 </button>
         </form>
-        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="bi bi-sliders"></i>
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Recent</a>
-            <a class="dropdown-item" href="#">Time</a>
-            <a class="dropdown-item" href="#">Date</a>
-        </div>
     </div>
   </div>
 </nav>
-<!-- Navbar End -->
 
 <!-- Weather Forecast -->
+<main class="container-sm">
 <div class="container mt-5 pt-5">
     <div class="card text-center">
         <div class="card-body">
@@ -45,15 +33,13 @@
             <p id="weatherInfo" class="mt-3">Weather Info will appear here</p>
         </div>
     </div>
-</div>
-
-<!-- News Section -->
+</div> 
 <section class="mt-4" style="min-height: 0px">
     <div class="container-sm">
         <!-- Card 1 -->
         <div class="card mb-4">
             <div class="card-header">Today's News!</div>
-            <img src="{{asset('/images/talent.png')}}" class="card-img-top" alt="Talent Show Image">
+            <img src="{{asset('/images/talent.png')}}" class="img-fluid" alt="talent">
             <div class="card-body">
                 <h5 class="card-title">Upcoming School Event</h5>
                 <p class="card-text">
@@ -75,64 +61,41 @@
                 </form>
             </div>
         </div>
+</main>
 
-        <!-- Card 2 -->
-        <div class="card mb-4">
-            <div class="card-header">Yesterday's News!</div>
-            <img src="{{asset('/images/tupad.png')}}" class="card-img-top" alt="TUPAD Program">
-            <div class="card-body">
-                <h5 class="card-title">TUPAD Program Enrollment</h5>
-                <p>Earn extra income with TUPAD! Enroll now for short-term jobs.</p>
-                <p><strong>Eligibility:</strong></p>
-                <ul>
-                    <li>Resident of Metro Manila</li>
-                    <li>At least 18 years old</li>
-                    <li>Willing to work for a specified number of days</li>
-                </ul>
-                <p><strong>How to Apply:</strong></p>
-                <ul>
-                    <li>Visit your local DOLE office</li>
-                    <li>Bring valid ID and proof of residency</li>
-                    <li>Submit the application form</li>
-                </ul>
-                <button class="btn btn-primary">Like</button>
-                <button class="btn btn-secondary">Share</button>
-                <button class="btn btn-info">Comment</button>
-                <form class="mt-2">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Enter your comment">
-                        <button class="btn btn-primary" type="submit">
-                            <i class="bi bi-send"></i>
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</section>
-
-<!-- Bottom Navbar -->
-<nav class="navbar fixed-bottom">
-        <div class="container d-flex justify-content-around">
+<div class="offcanvas offcanvas-start show text-bg-dark" tabindex="-1" id="offcanvasDark" aria-labelledby="offcanvasDarkLabel">
+  <div class="offcanvas-header">
+  <img src = "/images/Logo.jpg" width ="50" height="50" alt = "Logo">
+    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvasDark" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <h5 class="offcanvas-title" id="offcanvasDarkLabel">Menu</h5>
             <a class="nav-link" href="#">
-                <img src="{{asset('/images/house.svg')}}" width="30" height="30" alt="Home">
+                <img src="{{asset('/images/house.svg')}}" alt="Home">
                 <div>Home</div>
             </a>
             <a class="nav-link" href="#">
-                <img src="{{asset('/images/Archive.svg')}}" width="30" height="30" alt="Archive">
+                <img src="{{asset('/images/Archive.svg')}}" alt="Archive">
                 <div>Archive</div>
             </a>
             <a class="nav-link" href="#">
-                <img src="{{asset('/images/pencil-square.svg')}}" width="30" height="30" alt="Write Post">
+                <img src="{{asset('/images/pencil-square.svg')}}" alt="Write Post">
                 <div>Write Post</div>
             </a>
             <a class="nav-link" href="#">
-                <img src="{{asset('/images/person-circle.svg')}}" width="30" height="30" alt="Account Settings">
+                <img src="{{asset('/images/person-circle.svg')}}" alt="Account Settings">
                 <div>Account</div>
             </a>
-        </div>
-</nav>
+    <h5 class="offcanvas-title" id="offcanvasDarkLabel">Filter Posts</h5>
+    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="bi bi-sliders"></i>
+    </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Recent</a>
+            <a class="dropdown-item" href="#">Time</a>
+            <a class="dropdown-item" href="#">Date</a>
+</div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="{{ asset('js/weatherapi.js')}}"></script>
 </body>
 </html>
