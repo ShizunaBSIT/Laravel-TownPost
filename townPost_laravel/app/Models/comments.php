8933bda59c066ae3e500f7fdb6b5f07df33ed529
ShizunaBSIT <msyokozawa@tip.edu.ph>
2024-11-27 21:37:14 +0800

    modify models

--- townPost_laravel/app/Models/comments.php
+++ townPost_laravel/app/Models/comments.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class comments extends Model
 {
     //
+    use HasFactory;
+    protected $table='comments';
+    protected $fillable=['post_ID','user_ID','content'];
 }
--- townPost_laravel/app/Models/reaction_img.php
+++ townPost_laravel/app/Models/reaction_img.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class reaction_img extends Model
 {
-    //
+    // we'll manually input the data in the database because this table is meant to store the
+    // filenames of different image files of reaction icons
+
+    use HasFactory;
+    protected $table='reaction_imgs';
 }
