fc07cf3838853ac01d6dc7708df3cdb25c65787b
itsmeEn <166961047+itsmeEn@users.noreply.github.com>
2024-11-28 09:29:42 +0800

    Revert "Yokozawa"

--- townPost_laravel/routes/web.php
+++ townPost_laravel/routes/web.php
@@ -1,37 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// controllers
-use App\Http\Controllers\postsControllers;
-use App\Http\Controllers\usersController;
-use App\Http\Controllers\commentsController;
-use App\Http\Controllers\reactionController;
-
-
 Route::get('/', function () {
-    return view('announcement');
+    return view('welcome');
 });
 
-/* Routing for postman -- TESTING PURPOSES -- */
-Route::get('/test/users/{id}',[usersController::class, 'viewUser']);
-Route::get('/test/users/create', [usersController::class,'createUser']);
-Route::get('/test/users/login', [usersController::class,'loginUser']);
-Route::get('/test/users/update', [usersController::class, 'updateUser']);
-Route::get('/test/users/delete', [usersController::class, 'deleteUser']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get ('/test/posts', [postsControllers::class, 'retrievePosts']);
-Route::get('/test/posts/{id}', [postsControllers::class, 'getPost']);
-Route::get('/test/posts/create', [postsControllers::class, 'createPost']);
-Route::get('/test/posts/update/{id}', [postsControllers::class, 'updatePost']);
-Route::get('/test/posts/delete/{id}', [postsControllers::class, 'deletePost']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/test/comments/{id}',[commentsController::class, 'viewComments']);
-Route::get('/test/comments/create', [commentsController::class, 'postComment']);
-Route::get('/test/comments/update',[commentsController::class, 'updateComment']);
-Route::get('/test/comments/delete/{id}',[commentsController::class, 'deleteComment']);
-
-Route::get('/test/reactions/{id}',[reactionController::class, 'getReactions']);
-Route::get('/test/reactions/react',[reactionController::class, 'react']);
-Route::get('/test/reactions/unreact',[reactionController::class, 'unreact']);
-/* ^^ Routing for postman ^^*/
+require __DIR__.'/auth.php';
